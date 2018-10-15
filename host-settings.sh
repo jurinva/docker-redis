70c32542364caca845f7c2d2b63e19c26edf6277
jurinva <jurinva@gmail.com>
2018-10-15 16:43:12 +0300

    Fix host setup script

--- host-settings.sh
+++ host-settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sysctl -w vm.overcommit_memory=1
-sysctl -w net.core.somaxconn=65535
-cp /etc/init.d/disable-transparent-hugepages
+sudo sysctl -w vm.overcommit_memory=1
+sudo sysctl -w net.core.somaxconn=65535
+sudo curl --progress-bar -L -o/etc/init.d/disable-transparent-hugepages https://raw.githubusercontent.com/jurinva/docker-redis/master/disable-transparent-hugepages
+sudo chown +x /etc/init.d/disable-transparent-hugepages
